9a130a3aceaa2b7b3ab0104e5f79a6e27e251708
HugoZedendahl <hugo.zedendahl@gmail.com>
2021-03-17 18:28:53 +0000

    added () to make the reset fuction not broken

--- assets/js/clusterData.js
+++ assets/js/clusterData.js
@@ -1,11 +1,20 @@
 var cluster1 = []
-var cluster2 = []
-var cluster3 = []
-var cluster4 = []
-var cluster5 = []
-var cluster6 = []
 
-function storeUrl ()
+function storeUrlCluster1 ()
     {
-
+        cluster1.length = 0
+        localStorage.getItem("cluster1Data")
+        newUrl = document.getElementById('cluster_1_link').value;
+        items.unshift(newUrl+localStorage.getItem("cluster1Data"))
+        localStorage.setItem("cluster1Data", JSON.stringify(cluster1));
+        return false
     }
+
+function resetCluster1 ()
+    {
+        localStorage.setItem("cluster1Data", 0);
+        return false
+    }
+
+/* return false found here https://stackoverflow.com/questions/47078498/javascript-add-value-from-input-box-to-array */
+/*json method found here https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage */
