c9ec907231a2e3d9df94cbd78e1cf866b85fdbc6
HugoZedendahl <hugo.zedendahl@gmail.com>
2021-03-18 21:20:25 +0000

    added updated functionality

--- assets/js/clusterData.js
+++ assets/js/clusterData.js
@@ -1,4 +1,13 @@
 var runtimeCluster = [];
+var number;
+
+$(".inputButton").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button
+        }
+    );
 
 function storeUrlCluster(number)
     {
@@ -14,12 +23,13 @@ function storeUrlCluster(number)
         return false;
     }
 
-function resetCluster1(resetCheck)
+function resetCluster()
     {
+        $('#cluster_'+number+'_reset:checkbox:checked').length > 0;
         if(resetCheck==true)
             {
                 localStorage.setItem("cluster"+number+"Data", 0);
-                return false;
+                return;
             }
 
     }
@@ -38,5 +48,20 @@ function launchCluster(number)
         close();
         /*closes the clusterpages site*/
     }
+
+function storeUrlCluster(number)
+    {
+        runtimeCluster = [];
+        /*Reset runtime cluster*/
+        oldCluster = JSON.parse(localStorage.getItem("cluster"+number+"Data"));
+        /* gets cluster data */
+        newUrl = document.getElementById("cluster_"+number+"_link").value;
+        /* gets new url */
+        runtimeCluster = oldCluster;
+        /*adds old cluster data into runtimeCluster*/
+        localStorage.setItem("cluster"+number+"Data", JSON.stringify(runtimeCluster));
+        return false;
+    }
+
 /* return false found here https://stackoverflow.com/questions/47078498/javascript-add-value-from-input-box-to-array */
 /*json method found here https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage */
