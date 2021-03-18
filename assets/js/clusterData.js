163ed82e8486591fff09b36a2439445a7c5e52c7
HugoZedendahl <hugo.zedendahl@gmail.com>
2021-03-18 20:39:54 +0000

    added javascript for core functions

--- assets/js/clusterData.js
+++ assets/js/clusterData.js
@@ -1,20 +1,42 @@
-var cluster1 = []
+var runtimeCluster = [];
 
-function storeUrlCluster1 ()
+function storeUrlCluster(number)
     {
-        cluster1.length = 0
-        localStorage.getItem("cluster1Data")
-        newUrl = document.getElementById('cluster_1_link').value;
-        items.unshift(newUrl+localStorage.getItem("cluster1Data"))
-        localStorage.setItem("cluster1Data", JSON.stringify(cluster1));
-        return false
+        runtimeCluster = [];
+        /*Reset runtime cluster*/
+        oldCluster = JSON.parse(localStorage.getItem("cluster"+number+"Data"));
+        /* gets cluster data */
+        newUrl = document.getElementById("cluster_"+number+"_link").value;
+        /* gets new url */
+        runtimeCluster = oldCluster;
+        /*adds old cluster data into runtimeCluster*/
+        localStorage.setItem("cluster"+number+"Data", JSON.stringify(runtimeCluster));
+        return false;
     }
 
-function resetCluster1 ()
+function resetCluster1(resetCheck)
     {
-        localStorage.setItem("cluster1Data", 0);
-        return false
+        if(resetCheck==true)
+            {
+                localStorage.setItem("cluster"+number+"Data", 0);
+                return false;
+            }
+
     }
 
+function launchCluster(number)
+    {
+        runtimeCluster = [];
+        /* resets runtime cluster */
+        runtimeCluster = JSON.parse(localStorage.getItem("cluster"+number+"Data"));
+        /*adds url's into runtimeCluster array*/
+        for (URL of runtimeCluster) 
+            {
+                window.open(URL);
+            }
+        /*launches websites*/
+        close();
+        /*closes the clusterpages site*/
+    }
 /* return false found here https://stackoverflow.com/questions/47078498/javascript-add-value-from-input-box-to-array */
 /*json method found here https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage */
