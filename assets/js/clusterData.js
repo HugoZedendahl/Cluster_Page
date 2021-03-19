db8fd5c6b46ad322fd5654936e315a85f86f92cc
HugoZedendahl <hugo.zedendahl@gmail.com>
2021-03-19 05:37:58 +0000

    added event listeners and name function

--- assets/js/clusterData.js
+++ assets/js/clusterData.js
@@ -1,25 +1,79 @@
 var runtimeCluster = [];
 var number;
-
+$( document ).ready(function() 
+{
+/* prevents functions to be launched before dom is ready*/
 $(".inputButton").click
     (function() 
         {
             var fired_button = $(this).val();
             number = fired_button
+
         }
     );
 
+$(".addURL").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            storeUrlCluster();
+        }
+    );
+
+$(".resetCluster").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            resetCluster();
+        }
+    );
+
+$(".nameCluster").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            nameClusterButton();
+        }
+    );
+
+$(".launch").click
+    (function() 
+        {
+            var fired_button = $(this).val();
+            number = fired_button;
+            launchCluster();
+        }
+    );
+});
+/* set's the value of number and launches function */
 function storeUrlCluster()
     {
         runtimeCluster = [];
         /*Reset runtime cluster*/
         oldCluster = JSON.parse(localStorage.getItem("cluster"+number+"Data"));
         /* gets cluster data */
+        if(oldCluster==null)
+            {
+                oldCluster = []
+            }
         newUrl = document.getElementById("cluster_"+number+"_link").value;
         /* gets new url */
-        runtimeCluster = oldCluster.unshift(newUrl);
-        /*adds old cluster data into runtimeCluster*/
+        runtimeCluster = oldCluster;
+        runtimeCluster = oldCluster;
+        if(runtimeCluster.length == 0) 
+            {
+                runtimeCluster = [newUrl];
+            } 
+        else 
+            {
+                runtimeCluster.push(newUrl)
+            }   
+        /*adds old cluster and the new url into runtimeCluster*/
         localStorage.setItem("cluster"+number+"Data", JSON.stringify(runtimeCluster));
+        /*stores runtimeCluster to local storage*/
         return;
     }
 
@@ -28,7 +82,8 @@ function resetCluster()
         /*checks confirmation checkbox*/
         if($('#cluster_'+number+'_reset:checkbox:checked').length > 0==true)
             {
-                localStorage.setItem("cluster"+number+"Data", 0);
+                newUrl= document.getElementById("cluster_"+number+"_name").value
+                localStorage.setItem("cluster"+number+"name", newUrl);
                 return;
             }
 
@@ -49,19 +104,10 @@ function launchCluster()
         /*closes the clusterpages site*/
     }
 
-function storeUrlCluster()
+function nameClusterButton()
     {
-        runtimeCluster = [];
-        /*Reset runtime cluster*/
-        oldCluster = JSON.parse(localStorage.getItem("cluster"+number+"Data"));
-        /* gets cluster data */
-        newUrl = document.getElementById("cluster_"+number+"_link").value;
-        /* gets new url */
-        runtimeCluster = oldCluster;
-        /*adds old cluster data into runtimeCluster*/
-        localStorage.setItem("cluster"+number+"Data", JSON.stringify(runtimeCluster));
-        return;
+        newName = 
+        localStorage.setItem("cluster"+number+"name", JSON.stringify(newName))
     }
-
 /* return false found here https://stackoverflow.com/questions/47078498/javascript-add-value-from-input-box-to-array */
 /*json method found here https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage */
